ca908b6efdc1a8cce3b7238bcf4aa0c5a4fc078f
gzarazaga <gzarazaga@gmail.com>
2023-07-26 14:13:19 -0300

    Redirect shorten url

--- src/main/java/com/upwork/shortener/controler/URLShortenerControler.java
+++ src/main/java/com/upwork/shortener/controler/URLShortenerControler.java
@@ -3,12 +3,14 @@ package com.upwork.shortener.controler;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
  
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.http.HttpStatus;
 
 import com.upwork.shortener.dto.URLShortenerDTO;
@@ -31,10 +33,11 @@ public class URLShortenerControler {
         return urlService.saveUrl(originalURL);
     }
         
-    @RequestMapping(value = "/reverse", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public URLShortenerDTO getURL(@RequestParam(value = "shortenedURL") String shortenedURL) throws URLNotFoundException, ExpiredUrlException {
-        return urlService.getURL(shortenedURL);
+    @GetMapping(value = "/reverse")
+    public RedirectView getURL(@RequestParam(value = "shortenedURL") String shortenedURL) throws URLNotFoundException, ExpiredUrlException {
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("http://" + urlService.getURL(shortenedURL).getOriginalURL());
+  
+        return redirectView;
     }
 }
